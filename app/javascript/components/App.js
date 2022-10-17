8627bd919f858f3ec6478766f4ba7edc163089df
David Vera Castillo <indigodavid@gmail.com>
2022-10-17 12:52:08 -0500

    🛠 Refactor components as project requirements

---
+++ app/javascript/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Greeting from './Greeting';
+
+const App = () => (
+  <Greeting />
+);
+
+export default App;
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greeting = () => (
+  <div>
+    Hola a todos
+  </div>
+);
+
+export default Greeting;

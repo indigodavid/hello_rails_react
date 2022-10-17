7ceae35d496d312b8dd9b07e96b9d7b95f152c2c
David Vera Castillo <indigodavid@gmail.com>
2022-10-17 17:48:32 -0500

    ✅ Add store to React App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Greeting from './Greeting';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
 
 const App = () => (
-  <Greeting />
+  <Provider store={store}>
+    <Greeting />
+  </Provider>
 );
 
 export default App;

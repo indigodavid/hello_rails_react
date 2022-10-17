39045e6281a4b4e76f633f73510bd2e55949f62d
David Vera Castillo <indigodavid@gmail.com>
2022-10-17 17:48:21 -0500

    ✅ Add random greeting to React component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingReducer';
 
-const Greeting = () => (
-  <div>
-    Hola a todos
-  </div>
-);
+const Greeting = () => {
+  const dispatch = useDispatch();
+
+  const greeting = useSelector((state) => state.data);
+  
+  useState(() => {
+    dispatch(fetchGreeting());
+  }, []);
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  )
+};
 
 export default Greeting;
